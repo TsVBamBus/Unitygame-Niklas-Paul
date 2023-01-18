e9ca3ebaee460e36a0916151cb672f037522ad2f
PaulWeGeSch <wehbergpaul@gmail.com>
2023-01-18 15:25:09 +0100

    LevelEnd
    - Funktionierendes level Ende
    - Tod bei Fallen von der Plattform

---
+++ IT 2D game 2.1/Assets/Scripts/GameManagement/LevelDeath.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDeath : MonoBehaviour
+{
+    public Collider2D D;
+    private void OnTriggerEnter2D(Collider2D D)
+    {
+        Debug.Log("Trigger");
+        SceneManager.LoadScene(0);
+    }
+}
--- IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
+++ IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public Collider2D C;
+    private void OnTriggerEnter2D(Collider2D C)
     {
+        Debug.Log("Trigger");
         SceneManager.LoadScene(0);
     }
 }
