aea4ea28ff79baf8bf4c56ab14ec02085e9c9065
PaulWeGeSch <wehbergpaul@gmail.com>
2023-01-20 12:35:59 +0100

    Exit Button Fix

--- IT 2D game 2.1/Assets/Scripts/Menu/MainMenu.cs
+++ IT 2D game 2.1/Assets/Scripts/Menu/MainMenu.cs
@@ -5,10 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(1);
-    }
     public void QuitGame()
     {
         Debug.Log("Quit");
