abae36fc93790aed4d1a3dbc42413d91cf3ffef5
PaulWeGeSch <wehbergpaul@gmail.com>
2023-01-23 17:43:40 +0100

    better jumping

---
+++ IT 2D game 2.1/Assets/Movment.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    Vector2 move;
+    public int speed;
+    bool m_FacingRight = true;
+
+    Rigidbody2D rb;
+
+    private void Update()
+    {
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        transform.Translate(move * speed * Time.deltaTime);
+
+        if (move.x > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+   
+        else if (move.x < 0 && m_FacingRight)
+        {
+           
+            Flip();
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(move * speed, ForceMode2D.Impulse);
+    }
+
+    private void Flip()
+    {
+  
+        m_FacingRight = !m_FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+   
+
+}
