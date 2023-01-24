287886c4345a987766c901f26e6f4379384ea0c3
PaulWeGeSch <wehbergpaul@gmail.com>
2023-01-24 16:27:41 +0100

    Musik + bugFix

--- IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
+++ IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
@@ -5,22 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
-    public Collider2D WinnerC;
-    public GameObject Winner;
 
+    public GameObject Winner;
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
     IEnumerator waiter()
     {
         yield return new WaitForSeconds(10);
     }
 
-    private void OnTriggerEnter2D(Collider2D W)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        W = WinnerC;
-        Winner.SetActive(true);
-        Time.timeScale = 0f;
-        StartCoroutine(waiter());
-        LevelAcces.level = SceneManager.GetActiveScene().buildIndex + 1;
-        SceneManager.LoadScene(0);       
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            Winner.SetActive(true);
+            Time.timeScale = 0f;
+            StartCoroutine(waiter());
+            LevelAcces.level = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(0);
+        }
+        if (other.gameObject.CompareTag("DeathBarrier"))
+        {
+            Debug.Log("Trigger");
+            SceneManager.LoadScene(0);
+        }
     }
     
 
