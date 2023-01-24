e241a272706654ebfa7d037aafb9232ca30ff5ad
PaulWeGeSch <wehbergpaul@gmail.com>
2023-01-24 15:38:49 +0100

    Bug fix

---
+++ IT 2D game 2.1/Assets/Scripts/GameManagement/LevelStart.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStart : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+   
+}
