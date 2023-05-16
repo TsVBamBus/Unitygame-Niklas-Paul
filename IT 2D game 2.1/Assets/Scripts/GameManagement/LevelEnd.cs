96bcfca4413e6a1c3dc3e0a3446e3894d7552dec
PaulWeGeSch <wehbergpaul@gmail.com>
2023-05-16 16:27:32 +0200

    Death Screen

--- IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
+++ IT 2D game 2.1/Assets/Scripts/GameManagement/LevelEnd.cs
@@ -5,35 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
-
-    public GameObject Winner; //Winner Screen
+    public GameObject winnerScreen; //Winner Screen
+    public GameObject looserScreen;
     private void Start()
     {
         // bei jedem Betreten der Scene wird TimeScale auf 1 gesetzt da er am Ende jeder Scene auf 0 gesetzt wird
         Time.timeScale = 1f; 
-    }
-    
-    IEnumerator waiter() //Erzeugung des Waiters
-    {
-        yield return new WaitForSeconds(10);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Finish")) //wenn Ziel erreicht wird
         {
-            Winner.SetActive(true);
             Time.timeScale = 0f;
+            winnerScreen.SetActive(true);
             StartCoroutine(waiter());
-            LevelAcces.level = SceneManager.GetActiveScene().buildIndex + 1;
-            SceneManager.LoadScene(0);
+            
         }
         if (other.gameObject.CompareTag("DeathBarrier")) //wenn die Todes Barriere berührt wird
         {
-            SceneManager.LoadScene(0);
+            GameLost();
         }
     }
-    
+
+    void GameLost()
+    {
+        Time.timeScale = 0f;
+        looserScreen.SetActive(true);
+    }
+
+    IEnumerator waiter() //Erzeugung des Waiters
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        LevelAcces.level = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(0);
+    }
+
 
 
 }
