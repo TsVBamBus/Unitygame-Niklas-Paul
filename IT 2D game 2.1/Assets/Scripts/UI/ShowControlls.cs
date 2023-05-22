66c7ea8ecdc033c4480e4afdc0013b3dc53c50d7
TsVBamBus <116377616+TsVBamBus@users.noreply.github.com>
2023-05-22 18:41:30 +0200

    Coins
    Level 1 Coins eingefügt

--- IT 2D game 2.1/Assets/Scripts/UI/ShowControlls.cs
+++ IT 2D game 2.1/Assets/Scripts/UI/ShowControlls.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowControlls : MonoBehaviour
-{
-    public GameObject ControllUI;
-    private void Start()
-    {
-        StartCoroutine(waiter());
-    }
+//public class ShowControlls : MonoBehaviour
+//{
+    //public GameObject ControllUI;
+    //private void Start()
+    //{
+        //StartCoroutine(waiter());
+    //}
     //zeigt die Controlls für 4 Sekunden
-    IEnumerator waiter()
-    {
-        yield return new WaitForSeconds(4);
-        ControllUI.SetActive(false);
-    }
-}
+    //IEnumerator waiter()
+    //{
+        //yield return new WaitForSeconds(4);
+        //ControllUI.SetActive(false);
+   // }
+//}
