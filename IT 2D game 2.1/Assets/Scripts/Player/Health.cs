0914ad26d07d6cb00f52b2d332439842574b1f06
PaulWeGeSch <wehbergpaul@gmail.com>
2023-05-23 18:20:16 +0200

    anderung

--- IT 2D game 2.1/Assets/Scripts/Player/Health.cs
+++ IT 2D game 2.1/Assets/Scripts/Player/Health.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Pathfinding;
 
 public class Health : MonoBehaviour
 {
     static public int Leben = 3;
+    Vector2 pBirdPush = new Vector2 (10f, 0f);
+    Vector2 nBirdPush = new Vector2(-10f, 0f);
+
+    public Rigidbody2D player;
+    public AIPath aIPath;
+
     public GameObject looserScreen;
     public GameObject playerUi;
+
     public GameObject Heart1;
     public GameObject Heart2;
     public GameObject Heart3;
 
-    public void Update()
+
+    public void FixedUpdate()
     {
         if (Leben <= 2) Heart3.SetActive(false);
         if (Leben <= 1) Heart2.SetActive(false);
@@ -23,12 +32,24 @@ public class Health : MonoBehaviour
         }
 
     }
-    public void OnTriggerEnter2D(Collider2D collision)
+
+    public void OnCollisionEnter2D(Collision2D collision)
     {
+        //Bei Collision mit Vogel
         if (collision.gameObject.CompareTag("EnemyBird"))
         {
-            Leben = Leben - 1;
-            Debug.Log(Leben);
+            Leben--;
+
+            if (aIPath.desiredVelocity.x > 0.01f)
+            {
+                player.AddForce(pBirdPush, ForceMode2D.Force);
+                Debug.Log("Push");
+            }
+            if (aIPath.desiredVelocity.x > -0.01f)
+            {
+                player.AddForce(nBirdPush, ForceMode2D.Force);
+                Debug.Log("Push");
+            }
         }
     }
 }
