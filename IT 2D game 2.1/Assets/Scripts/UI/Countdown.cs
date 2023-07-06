afbd45730568e6804db93a88aa5b633af94b5045
TsVBamBus <116377616+TsVBamBus@users.noreply.github.com>
2023-07-06 14:33:11 +0200

    Countdown

---
+++ IT 2D game 2.1/Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 240f;
+
+    
+
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime; 
+    }
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            
+        }
+    }
+
+
+}
